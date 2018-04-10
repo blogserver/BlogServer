299fcad23137517aacc5c55ed07b68129183f387
liqiwei <liqiwei123@outlook.com>
2018-04-10 09:06:56 +0800

    fix bug

--- api/src/main/java/com/opensource/soft/BlogServer/api/blog/controller/BlogController.java
+++ api/src/main/java/com/opensource/soft/BlogServer/api/blog/controller/BlogController.java
@@ -1,10 +1,12 @@
 package com.opensource.soft.BlogServer.api.blog.controller;
 
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.LineNumberReader;
+
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpRequest;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -35,8 +37,56 @@ public class BlogController{
 	 */
 	@RequestMapping(value = "/visit" , method = RequestMethod.POST)
 	public String visit(HttpServletRequest request,Visit visit) {
+		String ip = getRemoteAddress(request);
+		String mac = getMACAddress(ip);
+		System.out.println("ip------------->"+ip+",mac---------------->"+mac);
+		visit.setIp(ip);
+		visit.setMac(mac);
 		blogService.visitHistory(visit);
 		return BaseResponse.successJson();
 	}
+
+	public String getRemoteAddress(HttpServletRequest request) {
+		String ip = request.getHeader("x-forwarded-for");  
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {  
+            ip = request.getHeader("Proxy-Client-IP");  
+        }  
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {  
+            ip = request.getHeader("WL-Proxy-Client-IP");  
+        }  
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {  
+            ip = request.getHeader("HTTP_CLIENT_IP");  
+        }  
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {  
+            ip = request.getHeader("HTTP_X_FORWARDED_FOR");  
+        }  
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {  
+            ip = request.getRemoteAddr();  
+        }  
+        return ip;  
+    }
+	public String getMACAddress(String ip) {
+        String str = "";
+        String macAddress = "";
+        try {
+            Process p = Runtime.getRuntime().exec("nbtstat -a " + ip);
+            InputStreamReader ir = new InputStreamReader(p.getInputStream());
+            LineNumberReader input = new LineNumberReader(ir);
+            for (int i = 1; i < 100; i++) {
+                str = input.readLine();
+                if (str != null) {
+                    //if (str.indexOf("MAC Address") > 1) {
+                    if (str.indexOf("MAC") > 1) {
+                        macAddress = str.substring(
+                                str.indexOf("=") + 2, str.length());
+                        break;
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace(System.out);
+        }
+        return macAddress;
+    }
 	
 }
